e2552722a560ec94fae9afcc345f9ba518a4f93a
3ba2ii <aghonem2011@gmail.com>
2021-09-20 15:48:58 +0200

    📦 NEW : Added styles for SSO buttons in both light and dark mode

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,14 +1,40 @@
 import { Button } from '@chakra-ui/button';
-import { Divider, Flex, Heading } from '@chakra-ui/layout';
+import { useColorMode, useColorModeValue } from '@chakra-ui/color-mode';
+import { Heading } from '@chakra-ui/layout';
 import React from 'react';
-import { FaApple, FaFacebook, FaFacebookF, FaHeart } from 'react-icons/fa';
+import { FaApple, FaFacebook, FaHeart } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
 import { Layout } from '../components/Layout';
 import registerS from '../styles/register.module.css';
 
 interface loginProps {}
 
+const CustomColorScheme = {
+  facebook: {
+    light: {
+      colorScheme: 'facebook',
+    },
+    dark: {
+      bg: 'facebook.500',
+      color: 'whiteAlpha.900',
+      _hover: { bg: 'same', filter: 'brightness(1.1)' },
+    },
+  },
+  apple: {
+    light: {
+      bg: 'black',
+      color: 'whiteAlpha.900',
+      _hover: { bg: 'same', filter: 'brightness(1.5)' },
+    },
+    dark: {
+      bg: 'whiteAlpha.900',
+      color: 'gray.900',
+      _hover: { bg: 'same', filter: 'brightness(1.3)' },
+    },
+  },
+};
 const RegisterPage: React.FC<loginProps> = ({}) => {
+  const { colorMode } = useColorMode();
   return (
     <Layout title={'Create Account and Join Paaws'}>
       <main className={registerS['main-page-content-wrapper']}>
@@ -25,10 +51,10 @@ const RegisterPage: React.FC<loginProps> = ({}) => {
           </Button>
           <Button
             borderRadius='lg'
-            colorScheme='facebook'
             boxShadow='sm'
             w={'100%'}
             leftIcon={<FaFacebook size='20px' />}
+            {...CustomColorScheme.facebook[colorMode]}
           >
             Sign up with Facebook
           </Button>
@@ -39,21 +65,17 @@ const RegisterPage: React.FC<loginProps> = ({}) => {
             leftIcon={<FaApple size='20px' />}
             w={'100%'}
             variant='solid'
+            {...CustomColorScheme.apple[colorMode]}
           >
             Sign up with Apple
           </Button>
 
-          <Flex flexDirection='row' alignItems='center'>
-            <Divider />
-            <small>or</small>
-            <Divider />
-          </Flex>
+          <p className='divider-with-centered-value'>or</p>
           <Button
             borderRadius='lg'
             boxShadow='sm'
             className={registerS['apple-button']}
             leftIcon={<FaHeart color={'red'} size='20px' />}
-            colorScheme='gray'
             w={'100%'}
             variant='solid'
           >
--- theme.tsx
+++ theme.tsx
@@ -8,6 +8,22 @@ const breakpoints = createBreakpoints({
   xl: '80em',
 });
 
+/* const AppleButton = {
+  // style object for base or default style
+  baseStyle: (props: any) => ({
+    bg: props.colorMode === 'dark' ? 'red.300' : 'red.500',
+  }),
+  // styles for different sizes ("sm", "md", "lg")
+  sizes: {},
+  // styles for different visual variants ("outline", "solid")
+  variants: {},
+  // default values for `size` and `variant`
+  defaultProps: {
+    size: '',
+    variant: '',
+  },
+}; */
+
 const theme = extendTheme({
   colors: {
     black: '#16161D',
