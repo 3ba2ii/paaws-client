71b0ee12d7da7f0280e854c3d6970a2ea1467e45
3ba2ii <aghonem2011@gmail.com>
2021-11-01 14:00:29 +0200

    fix: 🐛 Fixed a bug in root margin in Intersection Observer

--- utils/useOnScreen.ts
+++ utils/useOnScreen.ts
@@ -10,7 +10,7 @@ export default function useOnScreen(ref: any) {
       observer = new IntersectionObserver(
         ([entry]) => setIntersecting(entry.isIntersecting),
         {
-          rootMargin: '300px 0px 0px 0px',
+          rootMargin: '0px 0px 200px 0px',
         }
       );
 
