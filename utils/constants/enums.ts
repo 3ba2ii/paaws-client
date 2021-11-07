3ca5b14e0aca5ab12443d1512058d08086c8311a
3ba2ii <aghonem2011@gmail.com>
2021-11-07 15:10:24 +0200

    feat: 🎸 Added Location selection enum

--- utils/constants/enums.ts
+++ utils/constants/enums.ts
@@ -1,7 +1,18 @@
-import { FaGlobeAmericas } from 'react-icons/fa';
 import { PrivacyType } from './../../generated/graphql';
 
 export const PrivacyTypeCustomized = Object.entries(PrivacyType)
+  .reverse()
+  .map(([key, value]) => ({
+    key,
+    value,
+  }));
+
+export enum SelectLocationOptions {
+  MAP = 'Select using Map',
+  MANUAL = 'Enter Manually',
+}
+
+export const SelectLocationObj = Object.entries(SelectLocationOptions)
   .reverse()
   .map(([key, value]) => ({
     key,
