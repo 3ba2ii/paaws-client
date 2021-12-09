12489c01dae55583dc4a7e44d4c4cf757f62ac31
3ba2ii <aghonem2011@gmail.com>
2021-12-09 15:54:03 +0200

    fix: 🐛 fix a warning 'Hooks must be called in the exact order'

--- pages/missing/[id].tsx
+++ pages/missing/[id].tsx
@@ -2,34 +2,31 @@ import { Button, VStack } from '@chakra-ui/react';
 import { LoadingComponent } from 'components/common/loading/LoadingSpinner';
 import { Layout } from 'components/Layout';
 import NotFound from 'components/NotFound';
-import { useMissingPostQuery } from 'generated/graphql';
+import { MissingPostQuery, useMissingPostQuery } from 'generated/graphql';
 import MissingPostContainer from 'modules/posts/missing/MissingPostContainer';
 import router, { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import withApollo from 'utils/withApollo';
 
 interface MissingPostProps {}
 
 const MissingPost: React.FC<MissingPostProps> = () => {
+  const [post, setPost] = React.useState<
+    MissingPostQuery['missingPost'] | null
+  >(null);
   const {
     query: { id },
   } = useRouter();
 
-  if (!id || Array.isArray(id))
-    return (
-      <NotFound
-        title='Invalid URL'
-        subtitle='This is and invalid url please make sure you are in the right page'
-      />
-    );
-
   const { data, loading } = useMissingPostQuery({
-    variables: { missingPostId: parseInt(id) },
+    variables: { missingPostId: parseInt(id as string) },
+    onCompleted: (d) => {
+      setPost(d.missingPost);
+    },
   });
 
-  const post = data?.missingPost;
-  const isFound = !loading && post?.missingPost && !post.errors?.length;
+  const isFound = !loading && post && !data?.missingPost.errors?.length;
 
   return (
     <Layout includeFooter={false}>
