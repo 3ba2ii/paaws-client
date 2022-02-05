6eb2688e2802baab094df6a144a965d94e386f5c
3ba2ii <aghonem2011@gmail.com>
2022-02-05 17:27:57 +0200

    feat: 🎸 added a redirect middleware for login page

---
+++ pages/login/_middleware.ts
@@ -0,0 +1,10 @@
+//This middleware will check if the user is logged in or not and redirect him to home page if he's already logged in
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const cookies = req.cookies;
+  if (cookies && cookies.qid) {
+    return NextResponse.redirect('/');
+  }
+  return NextResponse.next();
+}
