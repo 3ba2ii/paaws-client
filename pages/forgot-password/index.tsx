e8fc1275d9a2f063245075f76989e89822668f01
3ba2ii <aghonem2011@gmail.com>
2022-02-07 16:03:19 +0200

    feat: 🎸 added another box to indicate that email has been sent

--- pages/forgot-password/index.tsx
+++ pages/forgot-password/index.tsx
@@ -1,8 +1,9 @@
+import { ChevronLeftIcon } from '@chakra-ui/icons';
 import { Heading, Text, Box, VStack, Input, Button } from '@chakra-ui/react';
 import Logo from 'components/Logo';
 import { useForgotPasswordMutation } from 'generated/graphql';
 import Image from 'next/image';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { isValidEmail } from 'utils/isValidEmail';
 import withApollo from 'utils/withApollo';
 
@@ -11,12 +12,16 @@ interface ForgotPasswordPageProps {}
 const ForgotPasswordPage: React.FC<ForgotPasswordPageProps> = ({}) => {
   const [sendPasswordResetEmail, { loading }] = useForgotPasswordMutation();
   const [email, setEmail] = React.useState('');
+  const [emailSent, setEmailSent] = useState(false);
 
   const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) =>
     setEmail(e.target.value);
 
   const onSubmit = async () => {
-    sendPasswordResetEmail({ variables: { identifier: email } });
+    const { data } = await sendPasswordResetEmail({
+      variables: { identifier: email },
+    });
+    setEmailSent(true);
   };
   useEffect(() => {
     document.title = 'Reset your password - Paaws';
@@ -37,32 +42,58 @@ const ForgotPasswordPage: React.FC<ForgotPasswordPageProps> = ({}) => {
           <Heading fontSize='xl' mb={4}>
             Reset your password
           </Heading>
+
           <Image
-            src={'/illustrations/girl-feeding-a-pet.svg'}
+            src={
+              emailSent
+                ? '/illustrations/undraw_mail_sent_re_0ofv.svg'
+                : '/illustrations/robot-fail.svg'
+            }
             width='500px'
             height={'350px'}
             alt='forgot-your-password'
           />
-          <VStack w='100%' px={7} spacing={4}>
-            <Text as='label' fontSize='md' fontWeight={'semibold'}>
-              Enter your account's email address and we will send you a password
-              reset link.
-            </Text>
-            <Input
-              placeholder='johndoe123@gmail.com'
-              value={email}
-              onChange={onChangeEmail}
-            />
-            <Button
-              onClick={onSubmit}
-              w='100%'
-              disabled={!isValidEmail(email)}
-              fontSize='sm'
-              isLoading={loading}
-            >
-              Send password reset email
-            </Button>
-          </VStack>
+          {emailSent ? (
+            <VStack w='100%' px={7} spacing={4}>
+              <Text as='label' fontSize='md' fontWeight={'semibold'}>
+                Check your email and your spam folder for a link to reset your
+                password. If it doesn't appear within a few minutes,{' '}
+                <Button
+                  variant='link'
+                  colorScheme={'blue'}
+                  fontWeight='bold'
+                  onClick={() => setEmailSent(false)}
+                >
+                  Click here
+                </Button>{' '}
+                to resend an email.
+              </Text>
+              <Button fontSize='sm' w='100%'>
+                Back to login page
+              </Button>
+            </VStack>
+          ) : (
+            <VStack w='100%' px={7} spacing={4}>
+              <Text as='label' fontSize='md' fontWeight={'semibold'}>
+                Enter your account's email address and we will send you a
+                password reset link.
+              </Text>
+              <Input
+                placeholder='johndoe123@gmail.com'
+                value={email}
+                onChange={onChangeEmail}
+              />
+              <Button
+                onClick={onSubmit}
+                w='100%'
+                fontSize='sm'
+                disabled={!isValidEmail(email)}
+                isLoading={loading}
+              >
+                Send password reset email
+              </Button>
+            </VStack>
+          )}
         </VStack>
       </Box>
     </VStack>
