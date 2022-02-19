f61abd955607d24f8cb5f6bc26158e629dc851b9
3ba2ii <aghonem2011@gmail.com>
2022-02-19 14:57:01 +0200

    feat: 🎸 added google login button and gets user's info

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,12 +2,24 @@ import { Heading } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/react';
 import { Layout } from 'components/Layout';
 import React from 'react';
+import { GoogleLogin, GoogleLoginResponse } from 'react-google-login';
 import { FcGoogle } from 'react-icons/fc';
 import styles from 'styles/login.module.css';
 import withApollo from 'utils/withApollo';
 import { LoginForm } from '../../modules/auth/login/LoginForm';
 
 const LoginPage: React.FC = () => {
+  const onSuccess = (response: GoogleLoginResponse) => {
+    console.log(
+      `🚀 ~ file: index.tsx ~ line 13 ~ onSuccess ~ response`,
+      response.getAuthResponse().id_token
+    );
+    //save the response to the context api
+    //If the user is already signed up then redirect to the home page
+
+    //If the user is not signed up then redirect to the register page to continue the sign up process
+  };
+  const onFailure = (response: any) => {};
   return (
     <Layout title='Welcome Back'>
       <div className={styles['login-page-container']}>
@@ -17,16 +29,27 @@ const LoginPage: React.FC = () => {
             👋
           </span>
         </Heading>
-        <Button
-          borderRadius='lg'
-          leftIcon={<FcGoogle size='20px' />}
-          w={'100%'}
-          variant='outline'
-          boxShadow='sm'
-          size='lg'
-        >
-          Continue with Google
-        </Button>
+        <GoogleLogin
+          clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID}
+          render={(renderProps) => (
+            <Button
+              borderRadius='lg'
+              leftIcon={<FcGoogle size='20px' />}
+              w={'100%'}
+              variant='outline'
+              boxShadow='sm'
+              size='lg'
+              {...renderProps}
+            >
+              Continue with Google
+            </Button>
+          )}
+          buttonText='Login'
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          cookiePolicy={'single_host_origin'}
+        />
+
         <p className='divider-with-centered-value'>or</p>
         <LoginForm />
       </div>
--- types/env.d.ts
+++ types/env.d.ts
@@ -1,7 +1,13 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_GOOGLE_API_KEY: string;
-    NEXT_PUBLIC_API_URL: string;
-    NEXT_PUBLIC_HOST_URL: string;
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_GOOGLE_API_KEY: string;
+      NEXT_PUBLIC_API_URL: string;
+      NEXT_PUBLIC_HOST_URL: string;
+      NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID: string;
+      NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_SECRET: string;
+    }
   }
 }
+
+export {}
