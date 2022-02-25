9f9e7e78082d390ba4dc921579fb8fec0368316b
3ba2ii <aghonem2011@gmail.com>
2022-02-25 18:41:26 +0200

    refactor: 💡 moved verify phone component into separate file

--- pages/profile/complete-info/phone-number.tsx
+++ pages/profile/complete-info/phone-number.tsx
@@ -1,38 +1,23 @@
-import {
-  Box,
-  Button,
-  Heading,
-  HStack,
-  PinInput,
-  PinInputField,
-  Text,
-  useToast,
-  VStack,
-} from '@chakra-ui/react';
-import InputHOC from 'components/common/input/CustomInputComponent';
-import InputField from 'components/common/input/InputField';
+import { Button, Heading, HStack, Text, VStack } from '@chakra-ui/react';
 import { Layout } from 'components/Layout';
 import Logo from 'components/Logo';
 import { UserAvatar } from 'components/UserAvatar';
-import { Form, Formik } from 'formik';
-import {
-  useSendOtpMutation,
-  useVerifyPhoneNumberMutation,
-} from 'generated/graphql';
 import { useIsAuth } from 'hooks/useIsAuth';
 import SendOTPComponent from 'modules/profile/complete-info/SendOTP';
-import { useRouter } from 'next/router';
+import VerifyOTPComponent from 'modules/profile/complete-info/VerifyPhoneNumber';
 import React, { useState } from 'react';
 import { GoVerified } from 'react-icons/go';
-import { toErrorMap } from 'utils/toErrorMap';
 import withApollo from 'utils/withApollo';
+
+interface PhoneNumberProps {}
+
 
 const CompleteInfoLeftCol = () => {
   return (
     <VStack
       w='100%'
       h='100%'
-      flex='.33'
+      flex='.4'
       align={'flex-start'}
       justify='center'
       px='65px'
@@ -47,97 +32,31 @@ const CompleteInfoLeftCol = () => {
     </VStack>
   );
 };
-interface PhoneNumberProps {}
+
+const AlreadyVerifiedComponent = () => {
+  return (
+    <VStack flex='.75' spacing={5}>
+      <GoVerified color='green' size={'42px'} />
+      <Heading size='sm' textAlign={'center'} maxW='50ch' lineHeight={'1.5'}>
+        You already have a verified phone number, If you want to change it,
+        Please head to the settings page and delete your phone number.
+      </Heading>
+    </VStack>
+  );
+};
 
 const VerifyPhoneNumberPage: React.FC<PhoneNumberProps> = () => {
   const { user } = useIsAuth();
-  const toaster = useToast();
-  const router = useRouter();
 
   const [step, setStep] = useState<'send-otp' | 'verify-otp'>('send-otp');
   const [phone, setPhone] = useState<string | null>(null);
 
-  const [verifyPhoneNumber] = useVerifyPhoneNumberMutation();
-
-  const userHasPhoneNumber: boolean = !!(
+  const hasVerifiedPhone: boolean = !!(
     user &&
     user.phone &&
     user.phoneVerified
   );
 
-  const VerifyOTPComponent = () => {
-    return (
-      <VStack w='100%' flex='.75'>
-        <Formik
-          initialValues={{ otp: '' }}
-          onSubmit={async ({ otp }, { setErrors }) => {
-            if (!otp || !otp.length || !phone) return;
-
-            /* send verification request */
-            const { data } = await verifyPhoneNumber({
-              variables: { otp, phone },
-            });
-
-            /* map the error */
-            if (data?.verifyPhoneNumber.errors?.length) {
-              const mappedErrors = toErrorMap(data?.verifyPhoneNumber?.errors);
-              return setErrors(mappedErrors);
-            }
-            /* show a toaster */
-            toaster({
-              status: 'success',
-              title: 'Phone Number Verified',
-              description: 'You rock! 🤘',
-              position: 'top-right',
-              variant: 'subtle',
-              isClosable: true,
-            });
-            /* redirect to the location page in case the user has no previous location*/
-            if (!user?.lat || !user?.lng) {
-              return router.push('/profile/complete-info/location');
-            }
-          }}
-        >
-          {({ isSubmitting, setFieldValue }) => (
-            <Form>
-              <VStack align='flex-start' spacing={5} maxW='450px'>
-                <InputHOC
-                  label={`A 4-digit number has been sent to this phone number${
-                    phone ? ' xxxxxxxx' + phone?.slice(-3) : ''
-                  }, Please type it in the box below `}
-                  name='otp'
-                  helperText='In case you didn’t receive an OTP, Click Resend OTP'
-                  required
-                >
-                  <HStack>
-                    <PinInput
-                      otp
-                      placeholder='😻'
-                      onChange={(val) => setFieldValue('otp', val.toString())}
-                    >
-                      <PinInputField />
-                      <PinInputField />
-                      <PinInputField />
-                      <PinInputField />
-                    </PinInput>
-                  </HStack>
-                </InputHOC>
-                <Button
-                  isLoading={isSubmitting}
-                  colorScheme={'teal'}
-                  type='submit'
-                  px={4}
-                  fontSize='sm'
-                >
-                  Verify Phone Number
-                </Button>
-              </VStack>
-            </Form>
-          )}
-        </Formik>
-      </VStack>
-    );
-  };
   return (
     <Layout
       title='Verify Phone Number'
@@ -159,7 +78,7 @@ const VerifyPhoneNumberPage: React.FC<PhoneNumberProps> = () => {
       </HStack>
       <HStack position={'absolute'} w='100%' h='100vh'>
         <CompleteInfoLeftCol />
-        {userHasPhoneNumber ? (
+        {!hasVerifiedPhone ? (
           step === 'send-otp' ? (
             <SendOTPComponent
               onSuccess={(p) => {
@@ -168,23 +87,12 @@ const VerifyPhoneNumberPage: React.FC<PhoneNumberProps> = () => {
               }}
             />
           ) : (
-            <VerifyOTPComponent />
+            phone && user && <VerifyOTPComponent {...{ phone, user }} />
           )
         ) : (
-          <VStack flex='.75' spacing={5}>
-            <GoVerified color='green' size={'42px'} />
-            <Heading
-              size='sm'
-              textAlign={'center'}
-              maxW='50ch'
-              lineHeight={'1.5'}
-            >
-              You already have a verified phone number, If you want to change
-              it, Please head to the settings page and delete your phone number.
-            </Heading>
-          </VStack>
+          <AlreadyVerifiedComponent />
         )}
-        {!userHasPhoneNumber && (
+        {!hasVerifiedPhone && (
           <Button
             pos={'absolute'}
             bottom='32px'
