d406ed4eddf3d097f0c393d76cabf6aeca13c138
3ba2ii <aghonem2011@gmail.com>
2022-04-06 15:06:14 +0200

    refactor: 💡 moved OwnedPetCard to a separate file

---
+++ modules/pet/OwnedPetCard.tsx
@@ -0,0 +1,34 @@
+import { Box, Text } from '@chakra-ui/react';
+import { UserOwnedPetsQuery } from 'generated/graphql';
+import React from 'react';
+interface IOwnedPetCardProps {
+  pet: UserOwnedPetsQuery['userOwnedPets']['ownedPets'][0]['pet'];
+}
+
+export const OwnedPetCard: React.FC<IOwnedPetCardProps> = ({ pet }) => {
+  return (
+    <Box
+      w='100%'
+      h='100%'
+      background={`url(${pet.thumbnail?.url}), #C4C4C4;`}
+      backgroundSize='contain'
+      css={{ boxShadow: 'inset 0px -50px 30px -11px rgba(0, 0, 0, 0.75);' }}
+      p={4}
+      display='flex'
+      flexDir='column'
+      justifyContent={'end'}
+      border='1px solid'
+      borderColor={'gray.50'}
+      cursor='pointer'
+      _hover={{ filter: 'brightness(50%)' }}
+      transition='filter .2s ease-in-out'
+    >
+      <Text fontSize='xl' fontWeight={'bold'} color='whiteAlpha.800'>
+        {pet.name}
+      </Text>
+      <Text fontSize='sm' fontWeight={'semibold'} color='whiteAlpha.800'>
+        10 months
+      </Text>
+    </Box>
+  );
+};
--- modules/pet/OwnedPetsGrid.tsx
+++ modules/pet/OwnedPetsGrid.tsx
@@ -1,50 +1,13 @@
-import {
-  Box,
-  Button,
-  GridItem,
-  SimpleGrid,
-  Text,
-  VStack,
-} from '@chakra-ui/react';
+import { Button, GridItem, SimpleGrid, VStack } from '@chakra-ui/react';
 import { LoadingComponent } from 'components/common/loading/LoadingSpinner';
-import { UserOwnedPetsQuery, useUserOwnedPetsQuery } from 'generated/graphql';
+import { useUserOwnedPetsQuery } from 'generated/graphql';
 import React from 'react';
+import { BsThreeDots } from 'react-icons/bs';
+import { OwnedPetCard } from './OwnedPetCard';
 
 interface OwnedPetsGridProps {
   userId: number;
 }
-
-interface IOwnedPetCardProps {
-  pet: UserOwnedPetsQuery['userOwnedPets']['ownedPets'][0]['pet'];
-}
-
-const OwnedPetCard: React.FC<IOwnedPetCardProps> = ({ pet }) => {
-  return (
-    <Box
-      w='100%'
-      h='100%'
-      background={`url(${pet.thumbnail?.url}), #C4C4C4;`}
-      backgroundSize='contain'
-      css={{ boxShadow: 'inset 0px -50px 30px -11px rgba(0, 0, 0, 0.75);' }}
-      p={4}
-      display='flex'
-      flexDir='column'
-      justifyContent={'end'}
-      border='1px solid'
-      borderColor={'gray.50'}
-      cursor='pointer'
-      _hover={{ filter: 'brightness(50%)' }}
-      transition='filter .2s ease-in-out'
-    >
-      <Text fontSize='xl' fontWeight={'bold'} color='whiteAlpha.800'>
-        {pet.name}
-      </Text>
-      <Text fontSize='sm' fontWeight={'semibold'} color='whiteAlpha.800'>
-        10 months
-      </Text>
-    </Box>
-  );
-};
 
 const OwnedPetsGrid: React.FC<OwnedPetsGridProps> = ({ userId }) => {
   const { data, fetchMore, loading, variables } = useUserOwnedPetsQuery({
@@ -90,7 +53,11 @@ const OwnedPetsGrid: React.FC<OwnedPetsGridProps> = ({ userId }) => {
         ) : null}
       </SimpleGrid>
       {data?.userOwnedPets.hasMore && (
-        <Button w='fit-content' onClick={fetchMorePets}>
+        <Button
+          leftIcon={<BsThreeDots />}
+          w='fit-content'
+          onClick={fetchMorePets}
+        >
           Load More
         </Button>
       )}
