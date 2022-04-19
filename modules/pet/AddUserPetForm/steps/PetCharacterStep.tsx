804d9b6727d131a28d719a002c8241e11ee90807
3ba2ii <aghonem2011@gmail.com>
2022-04-19 15:46:30 +0200

    style: 💄 improved ui of second step of adding user pet

--- modules/pet/AddUserPetForm/steps/PetCharacterStep.tsx
+++ modules/pet/AddUserPetForm/steps/PetCharacterStep.tsx
@@ -1,13 +1,33 @@
-import { VStack } from '@chakra-ui/react';
+import { Box, Text, Heading, VStack } from '@chakra-ui/react';
 import InputField from 'components/common/input/InputField';
 import React from 'react';
 import { StepProps } from './TellUsMoreStep';
 
 const PetCharacterStep: React.FC<StepProps> = ({ formik }) => {
   return (
-    <VStack>
-      <InputField label='About' name='about' textarea />
-      <InputField label='Birthdate' name='birthDate' type={'date'} />
+    <VStack align='flex-start' w='100%' h='100%' p={'32px'} spacing='24px'>
+      <Box>
+        <Heading fontSize={'24px'} size='lg' color='gray.700'>
+          Add character to your pet
+        </Heading>
+        <Text textStyle='p1'>
+          Make your pet stand out from the crowd. Tell us a little bit about
+        </Text>
+      </Box>
+      <InputField
+        label='About'
+        name='about'
+        textarea
+        placeholder='Meet Charlie! She is a ball-catching superstar who loves sports and spending time with friend...'
+      />
+      <Box w='calc(50% - 12px)'>
+        <InputField
+          label='Birthdate'
+          name='birthDate'
+          type={'date'}
+          color='gray.500'
+        />
+      </Box>
     </VStack>
   );
 };
