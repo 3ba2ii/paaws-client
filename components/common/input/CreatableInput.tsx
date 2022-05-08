2ae2392464eafdd1f80da51cd8189eaff7639b9f
3ba2ii <aghonem2011@gmail.com>
2022-05-08 19:29:37 +0200

    feat: 🎸 implemented a creatable select component

---
+++ components/common/input/CreatableInput.tsx
@@ -0,0 +1,69 @@
+import { Box } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { ActionMeta, OnChangeValue, Props } from 'react-select';
+import CreatableSelect from 'react-select/creatable';
+import { MyOptionType } from 'types/MyOptionType';
+
+interface CreatableInputProps {
+  options: MyOptionType[];
+  onChange: (value: any) => void;
+  value?: MyOptionType[];
+  creatableProps?: Props;
+}
+const createOption = (label: string): MyOptionType => ({
+  label,
+  value: label.toLowerCase().replace(/\W/g, '-'),
+});
+
+const CreatableInput: React.FC<CreatableInputProps> = ({
+  options,
+  value,
+  onChange,
+  creatableProps,
+}) => {
+  const [optionsState, setOptionsState] = useState<MyOptionType[]>([
+    ...options,
+  ]);
+
+  const handleChange = (
+    newValue: OnChangeValue<MyOptionType, true>,
+    actionMeta: ActionMeta<MyOptionType | unknown>
+  ) => {
+    console.group('Value Changed');
+    console.log(newValue);
+    console.log(actionMeta);
+    console.groupEnd();
+    onChange(newValue);
+  };
+
+  const handleCreateOption = (inputValue: string) => {
+    const newOption = createOption(inputValue);
+    setOptionsState([...optionsState, newOption]);
+    handleChange([...(getValue() || []), newOption], {
+      action: 'create-option',
+      option: newOption,
+    });
+  };
+
+  const getValue = () => {
+    const isMulti = creatableProps?.isMulti ?? false;
+    if (isMulti) {
+      return value;
+    }
+    return [];
+  };
+
+  return (
+    <Box w='100%' h='100%'>
+      <CreatableSelect
+        isClearable
+        options={optionsState}
+        onChange={handleChange as any}
+        onCreateOption={handleCreateOption}
+        value={getValue()}
+        {...creatableProps}
+      />
+    </Box>
+  );
+};
+export default CreatableInput;
