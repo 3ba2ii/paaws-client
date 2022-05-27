3f63b23b3bf56a47264872f98c962c5aebf316b4
3ba2ii <aghonem2011@gmail.com>
2022-05-27 19:45:50 +0200

    feat: 🎸 get the value of the slider based on user data

--- pages/profile/complete-info/index.tsx
+++ pages/profile/complete-info/index.tsx
@@ -1,11 +1,17 @@
+import { CheckIcon } from '@chakra-ui/icons';
 import {
   Box,
+  Button,
   Heading,
+  HStack,
   Slider,
   SliderFilledTrack,
   SliderTrack,
+  Text,
   VStack,
 } from '@chakra-ui/react';
+import { LoadingComponent } from 'components/common/loading/LoadingSpinner';
+import { useIsAuth } from 'hooks/useIsAuth';
 import CompleteInfoLayout from 'modules/profile/complete-info/layout';
 import React from 'react';
 import withApollo from 'utils/withApollo';
@@ -13,6 +19,7 @@ import withApollo from 'utils/withApollo';
 type CompleteInfoSteps = 'location' | 'phone' | 'avatar' | 'personalInfo';
 
 const CompleteInfo: React.FC = () => {
+  const { user, loading } = useIsAuth();
   const [steps, setSteps] = React.useState<{
     [key in CompleteInfoSteps]: boolean;
   }>({
@@ -31,6 +38,24 @@ const CompleteInfo: React.FC = () => {
 
     return (total / Object.keys(steps).length) * 100;
   };
+
+  React.useEffect(() => {
+    //update the initial state based on user data
+    if (user) {
+      const { avatar, bio, phone, phoneVerified, lat, lng } = user;
+      setSteps({
+        ...steps,
+        avatar: !!(avatar && avatar.url),
+        location: !!(lat && lng),
+        phone: !!(phone && phoneVerified),
+        personalInfo: !!(bio && bio !== ''),
+      });
+    }
+  }, [user, loading]);
+
+  if (loading) {
+    return <LoadingComponent />;
+  }
   return (
     <CompleteInfoLayout pageTitle='Complete your profile'>
       <VStack
@@ -40,6 +65,7 @@ const CompleteInfo: React.FC = () => {
         align='flex-start'
         maxW='600px'
         spacing={'24px'}
+        px='24px'
       >
         <Box w='100%'>
           <Heading size='md'>
@@ -56,6 +82,37 @@ const CompleteInfo: React.FC = () => {
             </SliderTrack>
           </Slider>
         </Box>
+        <VStack w='100%'>
+          <HStack
+            w='100%'
+            h='100%'
+            minH='110px'
+            border='2px solid'
+            borderColor='gray.200'
+            borderRadius={'6px'}
+            px='24px'
+            py='12px'
+            spacing={'24px'}
+            bg='transparent'
+            boxShadow={'sm'}
+            as={Button}
+          >
+            <CheckIcon fontSize='24px' color='green.500' />
+            <VStack w='100%' align='flex-start'>
+              <Heading size='sm' color={'blue.500'} fontWeight='semibold'>
+                Set your profile picture
+              </Heading>
+              <Text
+                textStyle='p1'
+                fontWeight='normal'
+                whiteSpace={'break-spaces'}
+                textAlign='left'
+              >
+                Your photo will make it easier for your friends to recognize you
+              </Text>
+            </VStack>
+          </HStack>
+        </VStack>
       </VStack>
     </CompleteInfoLayout>
   );
