eb548606a6e89b870c167613624fffb494a32a83
3ba2ii <aghonem2011@gmail.com>
2022-05-30 19:35:50 +0200

    feat: 🎸 added a fn declare if its a complete profile or not

--- utils/getUrlBasedOnUserInfo.ts
+++ utils/getUrlBasedOnUserInfo.ts
@@ -1,4 +1,5 @@
 import { MeQuery } from 'generated/graphql';
+import { isCompleteProfile } from './isCompletedProfile';
 export const getUrlBaseOnUserInfo = (
   user: MeQuery['me'],
   currentUrl:
@@ -12,26 +13,11 @@ export const getUrlBaseOnUserInfo = (
   if (!user) {
     return '/';
   }
-  if (currentUrl === '/profile/complete-info') {
+
+  const isCompleted = isCompleteProfile(user);
+
+  if (currentUrl === '/profile/complete-info' || isCompleted) {
     return '/';
   }
-  return 'profile/complete-info';
-
-  /* 
-  const { bio, phoneVerified, phone, lat, lng } = user;
-  
-  if (!phoneVerified && !phone && currentUrl !== 'phone-number') {
-    //if the user did not verify his phone number then redirect to the verify phone number page
-    return '/profile/complete-info/phone-number';
-  }
-  //otherwise redirect the user to the bio step
-  if ((!bio || bio === '') && currentUrl !== 'bio') {
-    return '/profile/complete-info/bio';
-  }
-
-  if ((!lat || !lng) && currentUrl !== 'location') {
-    return '/profile/complete-info/location';
-  }
-
-  return '/'; */
+  return '/profile/complete-info';
 };
