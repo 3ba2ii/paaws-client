b7ad361c535d62b7217a398e7252d545b4fc48f1
3ba2ii <aghonem2011@gmail.com>
2022-07-13 16:14:06 +0200

    refactor: 💡 refactored settings layout to in a separate file

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,89 +1,21 @@
-import {
-  Button,
-  Divider,
-  Heading,
-  Link,
-  SimpleGrid,
-  VStack,
-} from '@chakra-ui/react';
-import { Layout } from 'components/common/Layout';
+import { Heading } from '@chakra-ui/react';
 import { LoadingComponent } from 'components/common/loading/LoadingSpinner';
-import { SideFooter } from 'components/SideFooter';
 import { useIsAuth } from 'hooks/useIsAuth';
 import AboutYouSettings from 'modules/settings/AboutYou';
-import { useRouter } from 'next/router';
+import SettingsPageLayout from 'modules/settings/layout';
 import React from 'react';
 import withApollo from 'utils/withApollo';
 
 interface SettingsPageProps {}
 
-const SettingsTabsList: { key: string; label: string; url: string }[] = [
-  { key: 'about-you', label: 'About you', url: '/settings' },
-  { key: 'security', label: 'Security', url: `/settings/security` },
-  { key: 'account', label: 'Account', url: '/settings/account' },
-  { key: 'connections', label: 'Connections', url: '/settings/connections' },
-  {
-    key: 'email-settings',
-    label: 'Email Settings',
-    url: '/settings/email-settings',
-  },
-  { key: 'preferences', label: 'Preferences', url: '/settings/preferences' },
-  {
-    key: 'notifications',
-    label: 'Notifications',
-    url: '/settings/notifications',
-  },
-  { key: 'display', label: 'Display', url: '/settings/display' },
-  { key: 'danger-area', label: 'Danger Area', url: '/settings/danger-area' },
-];
 const SettingsPage: React.FC<SettingsPageProps> = ({}) => {
   const { user, loading } = useIsAuth();
-  const { pathname } = useRouter();
   if (loading) return <LoadingComponent />;
   if (!user) return <Heading>You are not logged in</Heading>;
   return (
-    <Layout title='Settings Page - Paaws' includeFooter={false}>
-      <SimpleGrid w='100%' gridTemplateColumns={'265px 4.5fr'} gap={14}>
-        <VStack
-          w='100%'
-          h='100%'
-          align='flex-start'
-          spacing={8}
-          pos='fixed'
-          maxWidth='265px'
-        >
-          <Heading fontSize='22px'>Settings</Heading>
-          <VStack align='flex-start' spacing={4}>
-            {SettingsTabsList.map(({ key, label, url }) => (
-              <Button
-                height={'fit-content'}
-                variant='unstyled'
-                as={Link}
-                key={key}
-                href={url}
-                fontSize='18px'
-                color={pathname === url ? 'blackAlpha.800' : 'blackAlpha.600'}
-                fontWeight={pathname === url ? 'semibold' : '400'}
-                _dark={{ color: 'whiteAlpha.800' }}
-              >
-                {label}
-              </Button>
-            ))}
-          </VStack>
-          <Divider />
-          <SideFooter />
-        </VStack>
-        <VStack
-          align='flex-start'
-          w='100%'
-          h='100%'
-          spacing={5}
-          gridColumn={'2/3'}
-        >
-          <AboutYouSettings user={user} />
-        </VStack>
-      </SimpleGrid>
-    </Layout>
+    <SettingsPageLayout user={user}>
+      <AboutYouSettings user={user} />
+    </SettingsPageLayout>
   );
 };
 export default withApollo(SettingsPage);
---
+++ pages/settings/security.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface SecuritySettingsProps {}
+
+const SecuritySettings: React.FC<SecuritySettingsProps> = ({}) => {
+  return <div>settings</div>;
+};
+export default SecuritySettings;
