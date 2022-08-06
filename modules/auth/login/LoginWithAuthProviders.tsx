7b2a7b00b15014cbfb60a5687b592d773164d829
3ba2ii <aghonem2011@gmail.com>
2022-08-06 17:47:59 +0200

    refactor: 💡 moved login with auth provider logic

--- modules/auth/login/LoginWithAuthProviders.tsx
+++ modules/auth/login/LoginWithAuthProviders.tsx
@@ -1,10 +1,16 @@
 import { Button } from '@chakra-ui/react';
+import {
+  LoginWithAuthProviderMutationResult,
+  ProviderTypes,
+  useLoginWithAuthProviderMutation,
+} from 'generated/graphql';
 import React from 'react';
 import GoogleLogin, { GoogleLoginResponse } from 'react-google-login';
 import { FcGoogle } from 'react-icons/fc';
+import { updateMeQueryCache } from 'utils/cache/updateMeQueryCache';
 
 interface LoginWithAuthProvidersProps {
-  onSuccess: Function;
+  onSuccess: (data: LoginWithAuthProviderMutationResult['data']) => void;
   onFailure: Function;
 }
 
@@ -12,6 +18,30 @@ const LoginWithAuthProviders: React.FC<LoginWithAuthProvidersProps> = ({
   onSuccess,
   onFailure,
 }) => {
+  const [externalLogin] = useLoginWithAuthProviderMutation();
+
+  const handleSuccess = async (response: GoogleLoginResponse) => {
+    const { tokenId } = response;
+
+    const { data } = await externalLogin({
+      variables: { provider: ProviderTypes.Google, providerId: tokenId },
+      update: (cache, { data: returnedData }) => {
+        if (!returnedData) return;
+        updateMeQueryCache(cache, returnedData?.loginWithAuthProvider?.user);
+      },
+    });
+
+    if (
+      !data ||
+      !data.loginWithAuthProvider.user ||
+      data.loginWithAuthProvider.errors?.length
+    ) {
+      onFailure();
+      return;
+    }
+    onSuccess && onSuccess(data);
+  };
+
   return (
     <GoogleLogin
       clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID}
@@ -29,7 +59,7 @@ const LoginWithAuthProviders: React.FC<LoginWithAuthProvidersProps> = ({
         </Button>
       )}
       buttonText='Login'
-      onSuccess={(response) => onSuccess(response as GoogleLoginResponse)}
+      onSuccess={(response) => handleSuccess(response as GoogleLoginResponse)}
       onFailure={() => onFailure()}
       cookiePolicy={'single_host_origin'}
     />
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,16 +1,11 @@
 import { Heading } from '@chakra-ui/layout';
 import { useToast } from '@chakra-ui/react';
 import { Layout } from 'components/common/Layout';
-import {
-  ProviderTypes,
-  useLoginWithAuthProviderMutation,
-} from 'generated/graphql';
+import { LoginWithAuthProviderMutationResult } from 'generated/graphql';
 import LoginWithAuthProviders from 'modules/auth/login/LoginWithAuthProviders';
 import router from 'next/router';
 import React from 'react';
-import { GoogleLoginResponse } from 'react-google-login';
 import styles from 'styles/login.module.css';
-import { updateMeQueryCache } from 'utils/cache/updateMeQueryCache';
 import { getUrlBaseOnUserInfo } from 'utils/getUrlBasedOnUserInfo';
 import withApollo from 'utils/withApollo';
 import { LoginForm } from '../../modules/auth/login/LoginForm';
@@ -21,28 +16,11 @@ interface LoginPageProps {
 
 const LoginPage: React.FC<LoginPageProps> = ({ title }) => {
   const toaster = useToast();
-  const [externalLogin] = useLoginWithAuthProviderMutation();
 
-  const onSuccess = async (response: GoogleLoginResponse) => {
-    const { tokenId } = response;
-
-    const { data } = await externalLogin({
-      variables: { provider: ProviderTypes.Google, providerId: tokenId },
-      update: (cache, { data: returnedData }) => {
-        if (!returnedData) return;
-        updateMeQueryCache(cache, returnedData?.loginWithAuthProvider?.user);
-      },
-    });
-
-    if (
-      !data ||
-      !data.loginWithAuthProvider.user ||
-      data.loginWithAuthProvider.errors?.length
-    ) {
-      onFailure();
-      return;
-    }
-    //check if the user verified his phone number or not
+  const onSuccess = async (
+    data: LoginWithAuthProviderMutationResult['data']
+  ) => {
+    if (!data) return onFailure();
     const redirectURL = getUrlBaseOnUserInfo(
       data.loginWithAuthProvider.user,
       'login'
