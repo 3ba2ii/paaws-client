82e2b66b2e53585db938a2eb88daf90d2b3b266e
3ba2ii <aghonem2011@gmail.com>
2022-08-08 23:57:00 +0200

    feat: 🎸 added useTimer hook

---
+++ hooks/useTimer.ts
@@ -0,0 +1,40 @@
+import { useRef, useState } from 'react';
+
+const useTimer = () => {
+  const [status, setStatus] = useState<'ON' | 'OFF'>('OFF');
+  const [countdown, setCountdown] = useState(0);
+
+  const intervalId = useRef<NodeJS.Timer | null>(null);
+
+  const start = (durationInSeconds: number) => {
+    if (status === 'ON' || intervalId.current !== null) {
+      throw new Error('Timer is already running');
+    }
+
+    const newIntervalId = setInterval(() => {
+      setCountdown(durationInSeconds);
+
+      if (--durationInSeconds < 0) {
+        clear();
+      }
+    }, 1000);
+
+    intervalId.current = newIntervalId;
+    setStatus('ON');
+    return { ok: true };
+  };
+  const clear = () => {
+    if (intervalId.current === null && status === 'OFF') {
+      return;
+    }
+    clearInterval(intervalId?.current as NodeJS.Timer);
+    intervalId.current = null;
+    setStatus('OFF');
+    setCountdown(0);
+    return { ok: true };
+  };
+
+  return { start, clear, countdown };
+};
+
+export default useTimer;
