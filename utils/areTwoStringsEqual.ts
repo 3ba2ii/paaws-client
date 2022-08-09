6380f1bcdd64785d099c3e3e81cc6fb044197595
3ba2ii <aghonem2011@gmail.com>
2022-08-09 14:00:46 +0200

    feat: 🎸 added a helper function to compare two strings

---
+++ utils/areTwoStringsEqual.ts
@@ -0,0 +1,5 @@
+//compare two strings
+const areTwoStringsEqual = (s1: string, s2: string): boolean => {
+  return s1.trim().toLowerCase() === s2.trim().toLowerCase();
+};
+export default areTwoStringsEqual;
